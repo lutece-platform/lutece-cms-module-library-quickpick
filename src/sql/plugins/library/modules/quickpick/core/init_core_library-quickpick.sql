00eda0529e27399e0a17068c3e1060dcdc087cdf
seboo <25958061+seboo@users.noreply.github.com>
2017-07-20 17:38:22 +0200

    first commit

---
+++ src/sql/plugins/library/modules/quickpick/core/init_core_library-quickpick.sql
@@ -0,0 +1,2 @@
+INSERT INTO `lutece_docpicker`.`core_datastore` (`entity_key`, `entity_value`) VALUES ('library.img.defaultSpace.id', '6');
+INSERT INTO `lutece_docpicker`.`core_datastore` (`entity_key`, `entity_value`) VALUES ('library.pdf.defaultSpace.id', '8');
